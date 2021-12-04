9859ad8d0052288877266b38ab352f28250c3c65
Ismatilloxon <marufkhonovismatillo@gmail.com>
2021-12-04 10:37:44 +0500

    add Musobaqa

--- Sport.uz/Spotr.uz/Views/Main/Search.swift
+++ Sport.uz/Spotr.uz/Views/Main/Search.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct Search: View {
     @State var searchText = ""
     @State var controlIndex = 0
+    @State var musobaqalar = ["10 - 17 mart Xorazim" : "17 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","23 - 30 mart namangan" : "15 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","10 - 17 aprel Toshkent" : "O'zbekiston kubogi","1 - 5 may Namangan" : "21 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","20 - 27 may Jizzax" : "24 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","1 - 4 iyyun Farg'ona" : "19 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","11 - 18 iyyul Nukus" : "14 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","1 - 8 sentabr Andijon" : "20 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","11 - 18 sentabr Buxoro" : "15 yoshgacha bo'lganlar o'rtasida O'zbekiston chempionati","10 - 17 oktabr Xorazim" : "barcha yosh toifasida O'zbekiston chempionati"]
+    var keys:[String] = []
+    var values:[String] = []
+    
+    init() {
+        for (key, value) in musobaqalar {
+            self.keys.append(key)
+            self.values.append(value)
+        }
+    }
     
     var body: some View {
         VStack(spacing: 10) {
@@ -19,6 +29,7 @@ struct Search: View {
             listOfData
             Spacer()
         }.navigationTitle("Search")
+            
     }
 }
 
@@ -52,7 +63,9 @@ extension Search {
     }
     
     private var listOfData: some View {
-        
+        VStack {
+        switch controlIndex {
+        case 0:
         List(1..<15, id: \.self) {index in
             Button(action: {
                 
@@ -67,6 +80,27 @@ extension Search {
                 }
             }
         }
-    }
+        case 1:
+            List(0..<keys.count, id: \.self) { index in
+                
+                VStack(alignment: .leading) {
+                        Text(keys[index])
+                            .foregroundColor(.black)
+                            .font(.system(size: 17))
+                            .multilineTextAlignment(.leading)
+                        Text(values[index])
+                            .font(.system(size: 14))
+                            .foregroundColor(.black.opacity(0.5))
+                            .multilineTextAlignment(.leading)
+                            .lineLimit(2)
+                    }.frame( height: 60)
+                
+            }
+        default:
+            Text("")
+        }
     
+        }
+    
+    }
 }
